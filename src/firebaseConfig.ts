332901e7a97a28e6b713895d191f9b6576c92a77
Manisrinivasa1999 <manisrinivasa1999@gmail.com>
2020-10-17 18:31:00 +0530

    initial setup

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDWpWwA9VH8-KRYqPpIkDR0aCuBVkVsNOo',
+  authDomain: 'job-portal-flipr.firebaseapp.com',
+  databaseURL: 'https://job-portal-flipr.firebaseio.com',
+  projectId: 'job-portal-flipr',
+  storageBucket: 'job-portal-flipr.appspot.com',
+  messagingSenderId: '626064367386',
+  appId: '1:626064367386:web:8ae227af162c5fcb9096a3',
+};
+
+export default firebaseConfig;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/auth';
+import { firebaseConfig } from '@/firebaseConfig';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const { storage } = firebase;
 
 export default new Vuex.Store({
   state: {
+    db,
+    firebase,
+    storage,
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
