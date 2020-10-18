f26ce5ced69e6e677c15b9923a30248c7d76ad47
Manisrinivasa1999 <manisrinivasa1999@gmail.com>
2020-10-18 07:10:15 +0530

    add signin page

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,14 @@ const routes: Array<RouteConfig> = [
     name: 'Home',
     component: Home,
   },
+  {
+    path: '/signin',
+    name: 'Signin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Signin" */ '../views/Signin.vue'),
+  },
 ];
 
 const router = new VueRouter({
