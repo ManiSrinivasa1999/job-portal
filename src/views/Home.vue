5e43463d559ad2821b230e0c7d48dc3bfe6ddd17
Manisrinivasa1999 <manisrinivasa1999@gmail.com>
2020-10-18 21:57:51 +0530

    fetch limited data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,38 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="(job, i) in jobs"
+        :key="i"
+      >
+        <v-card>
+          <v-card-title>
+            {{ job.jobtitle }}
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    this.$store.state.db.collection('job-data')
+      .limit(5)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.jobs.push(doc.data());
+        });
+      });
+  },
+};
 </script>
 
 <style></style>
